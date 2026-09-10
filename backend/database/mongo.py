4613034b62abe3fcc55ba41763fb5a84e7758d21
Barbara <barbi.medica@gmail.com>
2026-09-10 14:57:01 +0200

    Uređena povezanost s MongoDB bazom za kolekciju users

--- backend/database/mongo.py
+++ backend/database/mongo.py
@@ -14,11 +14,10 @@ client = MongoClient(
 db = client["zavrsni_rad"]
 
 
-
+users = db["users"]
 predictions = db["predictions"]
-
 analyses = db["analyses"]
-
 notes = db["notes"]
-
 subjects = db["subjects"]
+
+users.create_index("email", unique=True)
